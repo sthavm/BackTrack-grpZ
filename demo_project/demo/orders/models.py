c6971cbed6f2ba8342740ad2bf5de8db7d0c1f56
rickychung2019 <57457425+rickychung2019@users.noreply.github.com>
2019-12-06 23:13:45 +0800

    Update models.py

--- demo_project/demo/orders/models.py
+++ demo_project/demo/orders/models.py
@@ -11,8 +11,7 @@ class Project(models.Model):
     projectID=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
-    def __str__(self):
-        return self.projectID
+
 
 class User(AbstractUser):
     is_manager = models.BooleanField('manager status', default=False)
